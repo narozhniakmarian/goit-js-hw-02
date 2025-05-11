cffb3ce42daee4c3a2bf4a7ef7e6f2fb6a7bbc58
narozhniakmarian <narozhniakmarian@gmail.com>
2025-05-11 17:22:44 +0200

    goit-js-hw-02

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,16 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
-    return totalWidth;
+
+function checkForSpam(message) {
+    if (message.toLowerCase().includes("sale") || message.toLowerCase().includes("spam")) {
+        return true; 
+    } else {
+        return false; 
+    }
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));  
+console.log(checkForSpam("JavaScript weekly newsletter"));  
+console.log(checkForSpam("Get best sale offers now!"));  
+console.log(checkForSpam("Amazing SalE, only tonight!"));  
+console.log(checkForSpam("Trust me, this is not a spam message"));  
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));  
+console.log(checkForSpam("[SPAM] How to earn fast money?"));  
